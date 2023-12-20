3f9dc7a5a6c753a4b0ca8274f585819f6ba139b5
Swarnava Chakraborty <swarnavachakraborty@Swarnavas-MacBook-Air.local>
2023-12-20 23:12:27 +0530

    first commit with spring initializr

---
+++ src/main/java/com/sclab/boot/paymentwalletapp/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.sclab.boot.paymentwalletapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "Welcome to Wallet App";
+    }
+}
---
+++ src/test/java/com/sclab/boot/paymentwalletapp/PaymentWalletAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sclab.boot.paymentwalletapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentWalletAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
