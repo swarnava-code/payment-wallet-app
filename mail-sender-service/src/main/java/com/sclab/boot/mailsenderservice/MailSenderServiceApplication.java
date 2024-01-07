24883f2986c3e29c386374fe98a0ff544b9230af
Swarnava Chakraborty <swarnavachakraborty@Swarnavas-MacBook-Air.local>
2024-01-07 10:50:58 +0530

    init new microservice:MailSenderServiceApplication

---
+++ mail-sender-service/src/main/java/com/sclab/boot/mailsenderservice/MailSenderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sclab.boot.mailsenderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailSenderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailSenderServiceApplication.class, args);
+	}
+
+}
---
+++ mail-sender-service/src/test/java/com/sclab/boot/mailsenderservice/MailSenderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sclab.boot.mailsenderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailSenderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
